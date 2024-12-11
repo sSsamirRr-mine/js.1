ce02cbcb739a406d0c1b4008dd5bfdf673c9b08f
sSsamirRr-mine <sssamirrr4@gmail.com>
2024-12-11 15:53:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let a = 10 + 10 + "10"  
+let b = 10 + "10" + 10  
+let c = 10 + 10 + +"10"  
+let d = 10 / -" "  
+let e = 10 / +"2,5"
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
+
+let name = prompt('name?')
+
+console.log(name);
